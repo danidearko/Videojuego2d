b5174e5ce09f11ca5dd3059bbaec0ff9ee7026c5
danidearko <danielddvf16@gmail.com>
2024-03-17 17:14:30 +0100

    Primer commit, orgulloso de ello, fin.

---
+++ Assets/Scripts/MovPersonaje.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPersonaje : MonoBehaviour
+{
+
+    public float multiplicador = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola Mariano, qué tal?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movTeclas = Input.GetAxis("Horizontal"); //(a -1f - d 1f)
+
+   /*
+    transform.position = new Vector3 (
+        transform.position.x+(movTeclas/100),
+        transform.position.y,
+        transform.position.z
+
+    ); */
+
+    float miDeltaTime = Time.deltaTime;
+
+    Debug.Log(Time.deltaTime);
+
+    transform.Translate(
+        movTeclas*(Time.deltaTime*multiplicador),
+        0,
+        0
+    );
+
+
+        //         Debug.Log(Input.GetAxis("Horizontal"));
+    }
+}
